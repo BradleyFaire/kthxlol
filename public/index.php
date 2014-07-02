98c0c2b075ac19aae3b7e30fcc033e68c6d062be
Bradley Faire <bradley.faire01@gmail.com>
2014-07-02 16:28:27 +1200

    first commit

---
+++ public/index.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>kthxlol.com</title>
+	<link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+	<nav>
+		<div class="">
+			<ul class="leftnav">
+				<li><a class="navbutton" href="index.php">Home</a></li>
+				<li><a class="navbutton" href="index.php">Forum</a></li>
+				<li>
+					<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top" class="donate">
+						<input type="hidden" name="cmd" value="_s-xclick">
+						<input type="hidden" name="hosted_button_id" value="FDREAUXZ574QJ">
+						<input class="navbutton donate" type="submit" name="submit" value="Donate here">
+						<!-- <img alt="" border="0" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" width="1" height="1"> -->
+					</form>
+				</li>
+			</ul>
+			<ul class="loginnav">
+				<li><a class="navbutton" href="index.php">Login</a></li>
+				<li><a class="navbutton" href="index.php">Register</a></li>
+			</ul>
+		</div>
+	</nav>
+	<div class="container">
+		<h2>Hello!</h2>
+		<p>Just some simple information for everyone who plays and chats with us!</p><br>
+
+		<h2>TeamSpeak Server</h2>
+		<p>Click <a href="ts3server://ts.kthxlol.com">here</a> to connect to TS or you can manually add "ts.kthxlol.com" to your bookmarks. There is no password or port required.</p><br>
+
+		<h2>Minecraft Crackpack Server</h2>
+		<p>We run a Minecraft server that's currently running Crackpack from the ATLauncher. The address is "kthxlol.com".</p><br>
+
+		<h2>Minecraft Control Panel</h2>
+		<p>Click <a href="http://www.kthxlol.com:8082">here</a> you lazy son of a bitch.</p><br>
+
+		<h2>Web Hosting Control Panel</h2>
+		<p>You probably don't need to click <a href="https://www.kthxlol.com:8083">here</a>, but maybe you do.</p>
+	</div>
+
+	<img class="image-botton-right" src="assets/img/logo.png" alt="logo" />
+</body>
+</html>
